572a75520cf7c8fd17f92c10b84ab8d0f741978c
Code Repository for Colin Ilgen <4216730+newtownradio@users.noreply.github.com>
2022-10-14 20:47:28 -0700

    Add files via upload
    first commit. printf() and scanf() and working with doubles and characters.

---
+++ printf_and_scanf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define PI 3.14159
+int main(void)
+{ 
+ double radius;
+ double surface;
+ printf("Enter radius length:");
+ scanf("%lf", &radius);
+printf("surface volume of a sphere is: %lf \n\n", surface = 4.0*radius*radius*radius/3.0 );
+return 0;
+}
